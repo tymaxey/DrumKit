14780a7ed7f5799b4ff2e65a43b7c507cfa62a09
Ty <ty.maxey@gmail.com>
2019-07-19 08:45:17 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-window.addEventListener('keydown', function (e) {
-    const audio = document.querySelector(`audio[data-key='${e.keycode}']`);
-    console.log(audio);
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+    if (!audio) return; //stops the function from running
+    audio.currentTime = 0; //rewinds to the start
+    audio.play();
+    key.classList.add('playing');
+};
 
 
-});
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return //skip it if it's not a transform
+    this.classList.remove('playing');
+
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound)
